d29034173dd5bcaa774a8e81da93cdb25d2e1037
Katherine Beame <katherine.beame@gmail.com>
2016-04-05 10:09:22 -0700

    bonus one and two added using process.argv

--- kat/lib/greet.js
+++ kat/lib/greet.js
@@ -1,3 +1,8 @@
 const greet = module.exports = function (name) {
-  return 'Hello ' +name;
-};
+    return 'Hello ' + name;
+  };
+
+  if (process.argv.length >= 2) {
+    var commandname = process.argv[2]
+    console.log(greet(commandname));
+  };
