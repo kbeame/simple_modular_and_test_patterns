0ff7a2e7946a8b98bf28888664d85d60cce24ddd
Katherine Beame <katherine.beame@gmail.com>
2016-04-06 09:28:36 -0700

    added mocha and watched all of the pertinent files

--- kat/gulpfile.js
+++ kat/gulpfile.js
@@ -1,5 +1,9 @@
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
+const mocha = require('gulp-mocha');
+
+var files = ['./lib/**/*.js', 'gulpfile.js', 'index.js'];
+var allFiles = ['./lib/**/*.js', 'gulpfile.js', 'index.js', './test/**/*test.js'];
 
 gulp.task('lint:test', function () {
   return gulp.src('./test/**/*test.js')
@@ -12,4 +16,23 @@ gulp.task('lint:test', function () {
     ]
   }))
   .pipe(eslint.format());
+});
+
+gulp.task('lint:js', function () {
+  return gulp.src(files)
+  .pipe(eslint({
+    'rules': {
+      'indent' :['error',2]
+    }
+  }))
+  .pipe(eslint.format());
+});
+
+gulp.task('mocha', function () {
+  return gulp.src('./test/**/*test.js', {read: false})
+  .pipe(mocha({reporter: 'nyan'}));
+});
+
+gulp.task('watchFiles', function () {
+  gulp.watch(allFiles, ['lint:js', 'lint:test', 'mocha']);
 });
